ca34a0e74f25afa9639519a362e5dc6b31c63aef
Christina Mosnick <cmosnick07@gmail.com>
2019-03-10 18:45:08 -0500

    Got query for bounds to work

--- model.js
+++ model.js
@@ -5,10 +5,11 @@
 
   Documentation: http://koopjs.github.io/docs/usage/provider
 */
-const request = require('request').defaults({gzip: true, json: true})
-const config = require('config')
+// const request = require('request').defaults({gzip: true, json: true})
+// const config = require('config')
 const strava = require('strava-v3')
 var polyline = require('@mapbox/polyline')
+const terraformer = require('terraformer')
 
 function Model (koop) {}
 
@@ -24,58 +25,88 @@ function Model (koop) {}
 // req.params.layer
 // req.params.method
 Model.prototype.getData = function (req, callback) {
+  // Get req params
+  if(req.query && req.query.geometry) {
+    const xmin = req.query.geometry.xmin;
+    const ymin = req.query.geometry.ymin;
+    const xmax = req.query.geometry.xmax;
+    const ymax = req.query.geometry.ymax;
 
-  // Get segment
-  strava.segments.get({id: 229781}, (err, payload) => {
-    if(err){
-      console.log(err);
-      callback(err);
-      return;
+    if(xmin && ymin && xmax && ymax){
+      // Convert to lat and long
+      const minPoint = terraformer.Tools.positionToGeographic([xmin, ymin]);
+      const maxPoint = terraformer.Tools.positionToGeographic([xmax, ymax]);
+      
+      // get segments within bounding box
+      strava.segments.explore({
+        bounds: [minPoint[1], minPoint[0], maxPoint[1], maxPoint[0]].join(","), 
+        activity_type: 'riding'
+      }, (err, payload) => {
+        if(err){
+          console.log(err);
+          callback(err);
+          return;
+        }
+    
+        if(payload.errors) {
+          console.log(payload.errors);
+          callback(payload.errors);
+          return;
+        }
+
+        if(payload.segments) {
+          var featureCollection = translateSegments(payload.segments);
+          callback(null, featureCollection);
+          return;
+        } else {
+          callback("no segments returned");
+          return;
+        }
+
+        console.log(payload);
+
+      });
     }
-
-    if(payload.errors) {
-      console.log(payload.errors);
-      callback(payload.errors);
-      return;
-    }
-
-    var singleGeoJson = translateSingle(payload);
-    var geoJson = {
-      type: "FeatureCollection",
-      features: [singleGeoJson]
-    };
-  
-    callback(null, geoJson);
-  });
-
-  function translateSingle(input) {
-    // Convert polyline geom in response from polyline to geojson
-    if(input.map && input.map.polyline) {
-      var geom = polyline.toGeoJSON(input.map.polyline) 
-    } else {
-      // No geom in returned item
-      var geom = null;
-    }
-
-    // Add all attributes and geojson into larger geojson
-    var geoJson = {type: "Feature"}
-    geoJson.properties = input;
-    geoJson.geometry = geom;
-    // geoJson.properties.idField = "id";
-    console.log(geoJson);
-
-    return geoJson;
+  } else {
+    callback("");
+    return;
   }
+}
 
-  // // Call the remote API with our developer key
-  // request(`https://developer.trimet.org/ws/v2/vehicles/onRouteOnly/false/appid/${key}`, (err, res, body) => {
-  //   if (err) return callback(err)
 
-  //   // translate the response into geojson
-  //   const geojson = translate(body)
+  // // Get segment
+  // strava.segments.get({id: 229781}, (err, payload) => {
+    
 
-  //   // Optional: cache data for 10 seconds at a time by setting the ttl or "Time to Live"
-  //   // geojson.ttl = 10
+    
+
+  //   var singleGeoJson = translateSingle(payload);
+  //   var geoJson = {
+  //     type: "FeatureCollection",
+  //     features: [singleGeoJson]
+  //   };
+  
+  //   callback(null, geoJson);
+  // });
+
+function translateSingle(input) {
+  // Convert polyline geom in response from polyline to geojson geom
+  // Add all attributes and geom into feature geojson
+  return {
+    type: "Feature",
+    properties: input,
+    geometry: (input.points) ? polyline.toGeoJSON(input.points) : null
+  };
+}
+
+function translateSegments(segments) {
+  // feature collection with trnaslated features and cached for 10 seconds
+  return {
+    type: "FeatureCollection",
+    features: segments.map(translateSingle),
+    ttl: 10
+  };
+}
 
   //   // Optional: Service metadata and geometry type
   //   // geojson.metadata = {
@@ -84,35 +115,31 @@ Model.prototype.getData = function (req, callback) {
   //   //   geometryType: 'Polygon' // Default is automatic detection in Koop
   //   // }
 
-  //   // hand off the data to Koop
-  //   callback(null, geojson)
-  // })
-}
 
-function translate (input) {
-  return {
-    type: 'FeatureCollection',
-    features: input.resultSet.vehicle.map(formatFeature)
-  }
-}
+// function translate (input) {
+//   return {
+//     type: 'FeatureCollection',
+//     features: input.resultSet.vehicle.map(formatFeature)
+//   }
+// }
 
-function formatFeature (inputFeature) {
-  // Most of what we need to do here is extract the longitude and latitude
-  const feature = {
-    type: 'Feature',
-    properties: inputFeature,
-    geometry: {
-      type: 'Point',
-      coordinates: [inputFeature.longitude, inputFeature.latitude]
-    }
-  }
-  // But we also want to translate a few of the date fields so they are easier to use downstream
-  const dateFields = ['expires', 'serviceDate', 'time']
-  dateFields.forEach(field => {
-    feature.properties[field] = new Date(feature.properties[field]).toISOString()
-  })
-  return feature
-}
+// function formatFeature (inputFeature) {
+//   // Most of what we need to do here is extract the longitude and latitude
+//   const feature = {
+//     type: 'Feature',
+//     properties: inputFeature,
+//     geometry: {
+//       type: 'Point',
+//       coordinates: [inputFeature.longitude, inputFeature.latitude]
+//     }
+//   }
+//   // But we also want to translate a few of the date fields so they are easier to use downstream
+//   const dateFields = ['expires', 'serviceDate', 'time']
+//   dateFields.forEach(field => {
+//     feature.properties[field] = new Date(feature.properties[field]).toISOString()
+//   })
+//   return feature
+// }
 
 module.exports = Model
 
